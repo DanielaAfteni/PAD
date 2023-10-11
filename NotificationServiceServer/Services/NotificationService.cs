92c8f0b0215e7b7eb6a884f62c18fe0f6d729473
ilietodirascu <ilietodirascu@gmail.com>
2023-10-11 19:56:28 +0300

    added notification service

---
+++ NotificationServiceServer/Services/NotificationService.cs
@@ -0,0 +1,35 @@
+﻿using RabbitMQUtils;
+using Grpc.Core;
+using System.Text.Json;
+using Grpc.Net.Client;
+using static System.Runtime.InteropServices.JavaScript.JSType;
+
+namespace NotificationServiceServer.Services
+{
+    public class NotificationService : Notification.NotificationBase
+    {
+        private readonly ILogger<NotificationService> _logger;
+        private readonly RabbitMqPublisher _rabbitMqPublisher;
+        public NotificationService (RabbitMqPublisher rabbitMqPublisher, ILogger<NotificationService> logger)
+        {
+            _logger = logger;
+            _rabbitMqPublisher = rabbitMqPublisher;
+
+        }
+        public override Task<LogReply> SaveLogToRabbit(LogRequest request, ServerCallContext context)
+        {
+            try
+            {
+                var stringRequest = JsonSerializer.Serialize(request);
+                var test = DateTimeOffset.FromUnixTimeSeconds(request.Time.Seconds).DateTime;
+                _rabbitMqPublisher.SendQueueAsyncAck("Notifications", stringRequest);
+                return Task.FromResult(new LogReply { IsSuccess = true });
+            }
+            catch(Exception ex) { 
+                _logger.LogError(ex.Message);
+                return Task.FromResult(new LogReply { IsSuccess=false });
+            }
+            
+        }
+    }
+}
