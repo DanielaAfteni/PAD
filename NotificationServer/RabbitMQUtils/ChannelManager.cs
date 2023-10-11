2b210b8936417c7bc24f57959fd9274b0a3abb3b
ilietodirascu <ilietodirascu@gmail.com>
2023-10-11 21:13:33 +0300

    Fixed some files

---
+++ NotificationServer/RabbitMQUtils/ChannelManager.cs
@@ -0,0 +1,57 @@
+﻿using RabbitMQ.Client;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RabbitMQUtils
+{
+    public class ChannelManager
+    {
+        private readonly List<ChannelRmq> _channels;
+        private readonly int _channelLimit;
+        private readonly long _limitProcessing;
+        private readonly ConnectionFactory _factory;
+
+        public ChannelManager(ConnectionFactory factory, int channelLimit,int limitProcessing)
+        {
+            _factory = factory;
+            _channelLimit = channelLimit;
+            _limitProcessing = limitProcessing;
+            _channels = new List<ChannelRmq>();
+        }
+
+        public void Publish(string message, string entityName, ChannelType chanelType)
+        {
+            if (!_channels.All(x => x._EntityName == entityName))
+            {
+                _channels.Add(new ChannelRmq(_factory, entityName, chanelType));
+            }
+
+            var sender =
+             _channels.FirstOrDefault(x => x._EntityName == entityName && x.CountProcessing < _limitProcessing);
+            var newSender = false;
+            if (sender == null && _channels.Count < _channelLimit)
+            {
+                sender = new ChannelRmq(_factory, entityName, chanelType);
+                newSender = true;
+                Console.WriteLine($"Create New Publisher {_channels.Count}");
+            }
+            else if (sender == null)
+            {
+                while (_channels.FirstOrDefault(x => x._EntityName == entityName && x.CountProcessing < _limitProcessing) == null)
+                {
+                    Thread.Sleep(1000);
+                }
+                sender = _channels.FirstOrDefault(x => x._EntityName == entityName && x.CountProcessing < _limitProcessing);
+            }
+
+            sender!.Publish(message);
+            if (newSender)
+            {
+                _channels.Add(sender);
+            }
+        }
+    }
+}
---
+++ NotificationServer/RabbitMQUtils/RabbitMqPublisher.cs
@@ -0,0 +1,37 @@
+﻿using Microsoft.Extensions.Options;
+using RabbitMQ.Client;
+using System.Collections.Concurrent;
+using System.Text;
+
+namespace RabbitMQUtils
+{
+    public class RabbitMqPublisher
+    {
+        private readonly ConnectionFactory factory;
+        private readonly int _channelLimit = 1;
+        private readonly int _processingLimit = 100;
+        private readonly ChannelManager _channelManager;
+
+        public RabbitMqPublisher(IOptions<RabbitMqOptions> options)
+        {
+            //factory = new ConnectionFactory
+            //{
+            //    Uri = new Uri(options.Value.ConnectionString!)
+            //};
+            factory = new ConnectionFactory
+            {
+                HostName = "localhost"
+            };
+            _channelManager = new ChannelManager(factory,_channelLimit,_processingLimit);
+        }
+        public void SendExchangeAsyncAck(string exchangeName, string message)
+        {
+            _channelManager.Publish(message, exchangeName, ChannelType.Exchange);
+        }
+        public void SendQueueAsyncAck(string queueName, string message)
+        {
+            _channelManager.Publish(message, queueName, ChannelType.Queue);
+        }
+
+    }
+}
