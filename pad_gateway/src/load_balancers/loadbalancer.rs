5362c34130247dd440962bba08e7b8f5bc4b7563
ilietodirascu <ilietodirascu@gmail.com>
2023-10-26 19:23:14 +0300

    Added gateway

--- pad_gateway/src/load_balancers/loadbalancer.rs
+++
@@ -1,4 +0,0 @@
-// use std::{pin::Pin, future::Future};
-
-// use reqwest::StatusCode;
-// pub fn balance()
--- pad_gateway/src/main.rs
+++ pad_gateway/src/main.rs
@@ -1,38 +1,39 @@
-#[path="handlers/my_handlers.rs"]
+#[path = "models/chat_gpt.rs"]
+mod chat_gpt;
+#[path = "handlers/my_handlers.rs"]
 mod my_handlers;
-#[path="load_balancers/loadbalancer.rs"]
-mod loadbalancer;
-use std::{fs::File, sync::Arc};
-use std::io::Read;
-use axum::{Router, routing::get};
-use my_handlers::weatherforecast_handler;
-use my_handlers::weatherforecast_handler_simple;
+use axum::{routing::post, Router};
+use my_handlers::chat_gpt_handler;
 use serde_derive::Deserialize;
-use tokio::sync::Mutex;
+use std::io::Read;
 use std::net::SocketAddr;
+use std::{fs::File, sync::Arc};
+use tokio::sync::Mutex;
+use tower::ServiceBuilder;
+use tower_http::trace::TraceLayer;
 #[tokio::main]
 async fn main() {
+    print!("hello");
     let mut config_file = File::open("config.toml").expect("Unable to open config file");
     let mut config_toml = String::new();
-    config_file.read_to_string(&mut config_toml).expect("Unable to read config file");
+    config_file
+        .read_to_string(&mut config_toml)
+        .expect("Unable to read config file");
 
-    let config:AppConfig = toml::from_str(&config_toml).expect("Failed to parse TOML");
+    let config: AppConfig = toml::from_str(&config_toml).expect("Failed to parse TOML");
     let urls = Arc::new(Mutex::new(config.addresses.into_iter().cycle()));
-   
-    let app = Router::new().route("/", get(|| {
-        
-        weatherforecast_handler(urls)
-    }))
-    .route("/simple", get(|| weatherforecast_handler_simple("http://localhost:7070/weatherforecast")));
-    let addr = SocketAddr::from(([127, 0, 0, 1], 8080));
+    let app = Router::new()
+        .route("/chat", post(chat_gpt_handler))
+        .with_state(urls)
+        .layer(ServiceBuilder::new().layer(TraceLayer::new_for_http()));
+    let addr = SocketAddr::from(([0, 0, 0, 0], 8080));
 
     axum::Server::bind(&addr)
         .serve(app.into_make_service())
         .await
-        .unwrap();  
+        .unwrap()
 }
-
 #[derive(Debug, Deserialize)]
 struct AppConfig {
     addresses: Vec<String>,
-}   
+}
