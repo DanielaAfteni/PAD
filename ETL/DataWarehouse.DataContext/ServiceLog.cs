e5fe8df3016a681bb8622f1d44850e077d20188c
ilietodirascu <ilietodirascu@gmail.com>
2023-12-08 20:32:04 +0200

    hopefully finished with etl

---
+++ ETL/DataWarehouse.DataContext/ServiceLog.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataWarehouse.DataContext
+{
+    public class ServiceLog
+    {
+        public string Id { get; set; } = string.Empty;
+        public string ServiceName { get; set; } = string.Empty;
+        public string ServiceMessage { get; set; } = string.Empty;
+        public DateTimeOffset CreatedAt { get; set; }
+    }
+}
